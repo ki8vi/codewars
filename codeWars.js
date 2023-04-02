e7d25a542d149f693ebcfd1f8fa3db08666fc434
ki8vi <oneofbln@gmail.com>
2023-04-02 17:17:40 +0600

    Who likes it?

--- codeWars.js
+++ codeWars.js
@@ -496,12 +496,28 @@
 // "identifier"   =>  "identifier"
 // ""             =>  ""
 //rot13
-const rot13 = (message) => {
-  const originalAlpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  const cipher = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
-  return message.replace(/[a-z]/gi, letter => cipher[originalAlpha.indexOf(letter)])
-}
- console.log(rot13("Privet")) 
+// const rot13 = (message) => {
+//   const originalAlpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//   const cipher = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+//   return message.replace(/[a-z]/gi, letter => cipher[originalAlpha.indexOf(letter)])
+// }
+//  console.log(rot13("Privet")) 
   //cevirg
 
-  
+  //Who likes it?
+  function likes(names) {
+    names = names || [];
+    switch(names.length){
+      case 0: return 'no one likes this'; break;
+      case 1: return names[0] + ' likes this'; break;
+      case 2: return names[0] + ' and ' + names[1] + ' like this'; break;
+      case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
+      default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+    }
+  }
+console.log(likes(["Peter"]))
+//   []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
