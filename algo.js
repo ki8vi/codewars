7706667723fb416bbafd589ec98a3b61117d4859
ki8vi <oneofbln@gmail.com>
2023-06-01 18:20:01 +0600

    selecting sort

--- algo.js
+++ algo.js
@@ -19,21 +19,41 @@
 // }
 // console.log(binary([1, 3, 5, 7, 8, 15, 22], 22))
 
-//сортировка выбором -- O(n * n)
-const findSmallest = (arr) => {
-    for(let i = 0; i < arr.length; i++) {
-        let min = Infinity;
-        let index = null;
-        for(let j = i; j < arr.length; j++) {
-            if(arr[j] < min) {
-                min = arr[j];
-                index = j;
+//selection - sort -- O(n * n)
+// const findSmallest = (arr) => {
+//     for(let i = 0; i < arr.length; i++) {
+//         let min = Infinity;
+//         let index = null;
+//         for(let j = i; j < arr.length; j++) {
+//             if(arr[j] < min) {
+//                 min = arr[j];
+//                 index = j;
+//             }
+//         }
+//         let buf = arr[i];
+//         arr[i] = min;
+//         arr[index] = buf
+//     }
+//     return arr
+// }
+// console.log(findSmallest([115, 33, 67,-3, 43, 2, 1, 444, 1500, -23]))
+const sortArr = (array) => {
+    let arr = [];
+    for(let i = 0; i < array.length; i++) {
+       min = i
+        for(let j = i + 1; j < array.length; j++) {
+            if(array[j] < array[min]) {
+                min = j
             }
         }
-        let buf = arr[i];
-        arr[i] = min;
-        arr[index] = buf
+        
+        let first = array[i];
+        array[i] = array[min]
+        array[min] = first
     }
-    return arr
+    
+    return array
 }
-console.log(findSmallest([115, 33, 67,-3, 43, 2, 1, 444, 1500, -23]))
+
+console.log(sortArr([33, 55, 3, 2, 293, 900, -2, -23, -333]))
+
