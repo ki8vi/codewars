ec8b72625abb2aff388149de338c4245d84ca8f4
ki8vi <oneofbln@gmail.com>
2023-06-19 19:29:22 +0600

    Tap Code Translation

--- codeWars.js
+++ codeWars.js
@@ -3532,4 +3532,17 @@
 // }
 // console.log(sumCubes(2))
 
-//
+//Tap Code Translation
+function tapCodeTranslation(text) {
+  let out = []
+  const taps = {
+    a: [1, 1], b: [1, 2], c: [1, 3], k: [1, 3], d:[1, 4], e: [1, 5], f: [2, 1], g: [2, 2], h: [2, 3], i: [2, 4], j: [2, 5], l: [3, 1], m: [3, 2], n: [3, 3], o: [3, 4], p: [3, 5], q: [4, 1], r: [4, 2], s: [4, 3], t: [4, 4], u: [4, 5], v: [5, 1], w: [5, 2], x: [5, 3], y: [5, 4], z: [5, 5]
+  }
+  for(let i = 0; i < text.length; i++) {
+     out.push(".".repeat(taps[text[i]][0]) + " " + ".".repeat(taps[text[i]][1]))
+  }
+  
+  return out.join(" ");
+}
+console.log(tapCodeTranslation("dot"))
+console.log(tapCodeTranslation("example"))
